1775d2f8e81c30d2127e3e45dac74f2737c9b0ea
ianbalijawa <ianbalijawa16@gmail.com>
2023-10-12 18:42:49 +0300

    refactor: remove caregivers module

--- src/modules/caregivers/index.ts
+++
@@ -1,9 +0,0 @@
-import makeCallback from "@/adapters/express-callback";
-import { Router } from "express";
-import makeGetCaregiversController from "./controllers/get-caregivers";
-
-const router = Router();
-
-router.get(`/`, makeCallback(makeGetCaregiversController()));
-
-export default router;
