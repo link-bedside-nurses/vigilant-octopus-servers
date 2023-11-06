1c2730e635edefdd32d8f0943e2ac60eeea15d93
ianbalijawa <ianbalijawa16@gmail.com>
2023-11-06 21:50:32 +0300

    feat: add payment model

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,11 +3,13 @@ import { getModelForClass } from '@typegoose/typegoose'
 import { Appointment } from '@/database/schemas/Appointment'
 import { Rating } from '@/database/schemas/Rating'
 import { User } from '@/database/schemas/User'
+import { Payment } from '@/database/schemas/Payment'
 
 export const db = Object.freeze({
 	appointments: getModelForClass(Appointment),
 	ratings: getModelForClass(Rating),
 	users: getModelForClass(User),
+	payments: getModelForClass(Payment),
 })
 
 export type DatabaseType = typeof db
