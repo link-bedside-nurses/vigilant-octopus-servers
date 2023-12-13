43923bae4c7fd4a6890aac8a5b5622b32a8fcf17
ianbalijawa <ianbalijawa16@gmail.com>
2023-12-13 10:55:48 +0300

    add ext redis url

--- src/redis/client.ts
+++ src/redis/client.ts
@@ -2,13 +2,15 @@ import logger from '@/utils/logger'
 import { createClient } from 'redis'
 
 export default async function createRedisClient() {
-	const client = await createClient()
-		.on('error', err => {
-			logger.info(err), process.exit(1)
-		})
-		.on('connect', () => logger.info('Initialised redis connection'))
-		.on('end', () => logger.info('Connection to redis has been closed'))
-		.on('reconnecting', () => logger.info('Connected to redis'))
+	const client = await createClient( {
+		url: "rediss://red-clslqllcm5oc73b83ab0:fn9P7Eh0YwT46AXwguo5YbA7e8rypTcS@frankfurt-redis.render.com:6379"
+	} )
+		.on( 'error', err => {
+			logger.info( err ), process.exit( 1 )
+		} )
+		.on( 'connect', () => logger.info( 'Initialised redis connection' ) )
+		.on( 'end', () => logger.info( 'Connection to redis has been closed' ) )
+		.on( 'reconnecting', () => logger.info( 'Connected to redis' ) )
 		.connect()
 
 	return client
