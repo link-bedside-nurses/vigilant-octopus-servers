fda6a2928e358132ca13aefe99a8b6e5501af56f
ianbalijawa <ianbalijawa16@gmail.com>
2024-07-09 12:25:18 +0300

    remove redundant cron-job

--- src/services/otp.ts
+++ src/services/otp.ts
@@ -1,5 +1,11 @@
 import redis from './redis';
-import cron from 'node-cron';
+import * as crypto from 'crypto';
+
+export function generateOTP() {
+	const buffer = crypto.randomBytes(3);
+	const otp = parseInt(buffer.toString('hex'), 16).toString().slice(0, 6);
+	return otp;
+}
 
 export async function storeOTP(key: string, otp: string): Promise<void> {
 	await redis.set(key, otp, 'EX', 120);
@@ -11,16 +17,4 @@ export async function getOTP(key: string): Promise<string | null> {
 
 export async function expireOTP(key: string): Promise<void> {
 	await redis.del(key);
-}
-
-cron.schedule('*/2 * * * *', async () => {
-	console.log('Running scheduled task.');
-});
-
-import * as crypto from 'crypto';
-
-export function generateOTP() {
-	const buffer = crypto.randomBytes(3);
-	const otp = parseInt(buffer.toString('hex'), 16).toString().slice(0, 6);
-	return otp;
 }
