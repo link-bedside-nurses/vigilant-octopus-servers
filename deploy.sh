41f22d1c3f1e740baff970db6e79f30a48b5a459
ianbalijawa <ianbalijawa16@gmail.com>
2024-07-10 19:46:43 +0300

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Navigate to your project directory
+cd /root/vigilant-octopus-servers
+
+git pull origin v1.1
+
+if [ -d "dist/" ]; then
+  rm -rf dist/
+  echo "Removed existing dist/ directory."
+else
+  echo "No dist/ directory found."
+fi
+
+npm run build
+
+pm2 reload linkbedsides
+
+pm2 logs
