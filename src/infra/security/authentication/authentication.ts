f4d0599a250098c17dca5be33a4317d37bfded9d
ianbalijawa <ianbalijawa16@gmail.com>
2025-01-18 10:02:23 +0300

    add a check for admin too

--- src/infra/security/authentication/authentication.ts
+++ src/infra/security/authentication/authentication.ts
@@ -7,6 +7,7 @@ import HTTPException from '../../../core/utils/exception';
 import { envars } from '../../../config/constants';
 import { PatientRepo } from '../../database/repositories/patient-repository';
 import { CaregiverRepo } from '../../database/repositories/caregiver-repository';
+import { AdminRepo } from '../../database/repositories/admin-repository';
 
 export default async function authenticate( request: Request, _response: Response, next: NextFunction ) {
 	if (
@@ -28,8 +29,9 @@ export default async function authenticate( request: Request, _response: Respons
 	// check if the user with this id exists
 	const patient = await PatientRepo.getPatientById( decoded.id );
 	const caregiver = await CaregiverRepo.getCaregiverById( decoded.id );
+	const admin = await AdminRepo.getAdminById( decoded.id );
 
-	if ( !patient && !caregiver ) {
+	if ( !patient && !caregiver && !admin ) {
 		console.log( 'user not found' );
 		return next( new HTTPException( 'User not found!', StatusCodes.UNAUTHORIZED ) );
 	}
